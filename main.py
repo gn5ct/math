c126b7fe0256d0c141e43556bf0ee796d5639476
gn5ct <79898034914@mail.ru>
2025-02-11 19:06:37 +0300

    1.1
    вывод ответа

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from src import consol_version, graphic_version
 
 graphic_version()
+
--- src/implementors/grafic_app.py
+++ src/implementors/grafic_app.py
@@ -4,16 +4,20 @@ import dearpygui.dearpygui as dpg
 class GUI:
     def __init__(self):
         self.example = random_primer()
+    def paint_answer(self):
+        user_answer=dpg.get_value(item='answer')
+        print(user_answer)
     def start(self):
         dpg.create_context()
         dpg.create_viewport(width=332, height=342)
         with dpg.font_registry():
             with dpg.font('./ofont.ru_Zeitmax.ttf', 20, tag='ofont'):
                 dpg.add_font_range_hint(dpg.mvFontRangeHint_Cyrillic)
-        with dpg.window():
+        with dpg.window(tag = 'main'):
             dpg.add_text(self.example)
-            dpg.add_input_float()
-            dpg.add_button(label='Здесь будет ответ')
+            dpg.add_input_float(tag='answer')
+            dpg.add_button(label='Проверить ответ', callback=self.paint_answer)
+        dpg.set_primary_window(window = 'main', value = True)
         dpg.bind_font('ofont')
         dpg.setup_dearpygui()
         dpg.show_viewport()
